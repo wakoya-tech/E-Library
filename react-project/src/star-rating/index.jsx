f6f09187073d646e1f4decab3062143674627d56
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2026-01-09 22:34:23 +0300

    update

--- react-project/src/star-rating/index.jsx
+++ react-project/src/star-rating/index.jsx
@@ -5,7 +5,6 @@ import "./style.css";
 export default function StarRating({ noOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
-
   return (
     <div className="star-rating">
       {[...Array(noOfStars)].map((_, index) => {
